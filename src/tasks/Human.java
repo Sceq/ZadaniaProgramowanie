d96bb97fdad56889d98cdeb74f981f28cedf16ce
Filip Kaniecki <f.kaniecki@outlook.com>
2023-02-02 23:07:52 +0100

    Zadanie 3/4

--- src/tasks/Human.java
+++ src/tasks/Human.java
@@ -1,11 +1,18 @@
 package tasks;
 
+import java.util.Date;
+
 public class Human {
+    private static final Double DEFAULT_SALARY = 10000.0;
     Animal pet;
     private String firstName;
     private String lastName;
     private Integer age;
+    private Car car;
 
+
+
+    private Double salary;
 
 
 
@@ -14,6 +21,7 @@ public class Human {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
+        this.salary = DEFAULT_SALARY;
     }
 
     public Car getCar() {
@@ -23,6 +31,23 @@ public class Human {
     public void setCar(Car car) {
         this.car = car;
     }
+    public Double getSalary() {
+        Date Data = new Date();
+        System.out.println("Ostatnie pobranie informacji o wypłacie odbyło się "+ Data.toString()+" i wynosiła "+this.salary);
+        return this.salary;
+    }
 
-    private Car car;
+    public void setSalary(Double salary) {
+        if(salary >=0){
+            this.salary = salary;
+            System.out.println("Nowe dane zostały wysłane do systemu księgowego.");
+            System.out.println("Uwaga! Konieczność odebrania aneksu do umowy od pani Hani z kadr!");
+            System.out.println("ZUS i US już wiedzą o zmianie wypłaty i nie ma sensu ukrywać dochodu");
+        }
+        else{
+            System.out.println("Błąd, wypłata nie może być ujemna.");
+        }
+    }
+
+
 }
